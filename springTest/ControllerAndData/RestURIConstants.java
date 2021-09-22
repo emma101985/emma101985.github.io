dd36ac5b1afc3d2042c87fcbb35063fadaa5ef4d
emma101985 <86267392+emma101985@users.noreply.github.com>
2021-09-22 09:01:16 -0700

    Create RestURIConstants.java

---
+++ springTest/ControllerAndData/RestURIConstants.java
@@ -0,0 +1,9 @@
+package com.springwebapp;
+
+public class RestURIConstants {
+	
+	public static final String GET_NAME = "api/beans/{name}";
+	public static final String GET_ALL = "api/beans/";
+	
+
+}
